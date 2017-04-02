3af5574954045d9602c5c39239968b1bfc022945
SantoshSwaraj <skushwaha.mca.bbau@gmail.com>
2017-04-02 14:26:38 +0530

    applicationContext Configured :)

--- StudentsBankOfIndia_HSVer/src/main/dao/main/dao/StudentsBankOfIndiaDaoImpl.java
+++ StudentsBankOfIndia_HSVer/src/main/dao/main/dao/StudentsBankOfIndiaDaoImpl.java
@@ -1,11 +1,21 @@
 package main.dao;
 
+
 import org.springframework.orm.hibernate3.HibernateTemplate;
 
 import main.model.Bank_Admin;
 
 public class StudentsBankOfIndiaDaoImpl implements StudentsBankOfIndiaDao {
 	private HibernateTemplate template;
+	
+	public HibernateTemplate getTemplate() {
+		return template;
+	}
+	
+	public void setTemplate(HibernateTemplate template) {
+		this.template = template;
+	}
+
 	@Override
 	public void saveBankAdmin(Bank_Admin bank_admin) {
 		template.save(bank_admin);
