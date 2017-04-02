20c488483d904e6a81081a0f41fe2c9164485ce1
SantoshSwaraj <skushwaha.mca.bbau@gmail.com>
2017-04-02 16:32:48 +0530

    java.lang.NoSuchMethodError: org.hibernate.SessionFactory.openSession()

--- StudentsBankOfIndia_HSVer/src/main/dao/main/dao/StudentsBankOfIndiaDaoImpl.java
+++ StudentsBankOfIndia_HSVer/src/main/dao/main/dao/StudentsBankOfIndiaDaoImpl.java
@@ -1,17 +1,19 @@
 package main.dao;
 
 
+
+
 import org.springframework.orm.hibernate3.HibernateTemplate;
 
 import main.model.Bank_Admin;
 
 public class StudentsBankOfIndiaDaoImpl implements StudentsBankOfIndiaDao {
 	private HibernateTemplate template;
-	
+
 	public HibernateTemplate getTemplate() {
 		return template;
 	}
-	
+
 	public void setTemplate(HibernateTemplate template) {
 		this.template = template;
 	}
